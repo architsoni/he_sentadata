8a164b68c50981b25f406968da7324fd7a272095
ShivaliPatel <shivuptl@gmail.com>
2015-07-23 00:48:25 +0530

    angular-Cookie module added.. On login cookie added.. For all http request , cookie is set..

--- dashboard/app.js
+++ dashboard/app.js
@@ -5,30 +5,33 @@ angular.module("senta-overview", []);
 angular.module("senta-capture", []);
 angular.module("senta-login", []);
 
-angular.module('senta-app', ["ui.router", "oc.lazyLoad", "senta-overview", "senta-capture", "senta-login"])
-    /*.factory('authInterceptor',function ($rootScope, $q, $window) {
-     return {
-     request: function (config) {
-     config.headers = config.headers || {};
-     if ($window.sessionStorage.token) {
-     config.headers.Authorization = 'Bearer ' + $window.sessionStorage.token;
-     }
-     return config;
-     },
-     response: function (response) {
-     if (response.status === 401) {
-
-     }
-     return response || $q.when(response);
-     }
-     };
-     }).config(function ($httpProvider) {
-     $httpProvider.interceptors.push('authInterceptor');
-     })*/
-    .run(function ($rootScope, $state, $window, $location) {
+angular.module('senta-app', ["ui.router", "oc.lazyLoad", "senta-overview", "senta-capture", "senta-login", 'ngCookies'])
+    .factory('authHttpInterceptor',
+        ['$cookies', '$location', '$q' , function ($cookies, $location, $q) {
+            return {
+                request: function ($config) {
+                    if ($cookies.get('sentaApp')) {
+//                        $config.headers['sentaApp'] = $cookies.get('sentaApp');
+                    }
+                    return $config;
+                },
+                response: function (response) {
+                    if (response.data.message == 'login form') {
+                        $cookies.remove('sentaApp');
+                        $location.path('/login');
+                        return $q.reject(response);
+                    }
+                    return response;
+                }
+            };
+        }])
+    .config(['$httpProvider', function ($httpProvider) {
+        $httpProvider.interceptors.push('authHttpInterceptor');
+    }])
+    .run(function ($rootScope, $state, $cookies, $location) {
 
         $rootScope.$on('$stateChangeStart', function (event, nextState, toParam, currentState) {
-            if (!$window.sessionStorage.token && (nextState.name != 'login' && nextState.name != 'signUp')) {
+            if (!$cookies.get('sentaApp') && (nextState.name != 'login' && nextState.name != 'signUp')) {
                 $rootScope.notLoginState = false;
                 $location.path('/login');
             } else if (nextState.name == 'login' || nextState.name == 'signUp') {
--- dashboard/modules/login/loginController.js
+++ dashboard/modules/login/loginController.js
@@ -4,17 +4,22 @@
 
 angular.module("senta-login")
     .controller('LoginController',
-        ["$scope", "$window", "$state", '$http', function ($scope, $window, $state, $http) {
+        ["$scope", "$window", "$state", '$http', "$cookies", function ($scope, $window, $state, $http, $cookies) {
 
             $scope.login = function (params) {
+                var json = 'WsJson=yes&WsJsonData={"login": {"username": "", "password": ""},' +
+                    '"operation": {"object":"XaUser", "event":"XaUserLogin"},' +
+                    '"params":[{"name":"email","value":"' + params.username + '"},{"name":"password","value":"' + params.password + '"}]}';
 
-                $http.get("https://demo1.sentadata.com/SentaDCaaS.cgi?obj=XaUser&evt=XaUserLogin&email=" + params.username + "&password=" + params.password)
+                $http.post("https://demo1.sentadata.com/SentaDCaaS.cgi", json)
                     .success(function (result) {
-                        $window.sessionStorage.token = "12345678910";
+                        $cookies.put('sentaApp', '12345678910');
+//                        $window.sessionStorage.token = "12345678910";
                         $state.transitionTo("overview");
                     })
                     .error(function (error) {
-                        delete $window.sessionStorage.token;
+                        $cookies.remove('sentaApp');
+//                        delete $window.sessionStorage.token;
                     })
 
                 /*if (params.username == 'senta' && params.password == 'senta') {
